cdb1c28e7a1f37b352b59807dcec31c0bd5e124a
Lily Mayer <150291424+lmayer5596@users.noreply.github.com>
2024-02-15 15:12:50 -0500

    adds comments

--- project/src/features/build_features.v2.R
+++ project/src/features/build_features.v2.R
@@ -1,17 +1,21 @@
-
+#reads in raw data
 test <- fread('./project/volume/data/raw/test_file.csv')
 train <- fread('./project/volume/data/raw/train_file.csv')
 submit <- fread('./project/volume/data/raw/samp_sub.csv')
 
+#creates a column of the sum of all variable row values, then saves only row_sum and result
 train$row_sum <- rowSums(train[, !c('id', 'result')])
 train <- train[, c('row_sum', 'result')]
 
+#creates a column of the sum of all variable row values, then saves row_sum and creates an empty results column
 test$row_sum <- rowSums(test[, !c('id')])
 test <- test[, c('row_sum')]
 test$result <- 0
 
+#empties the result column for submission
 submit$result <- 0
 
+#saves the edited datasets into the interim folder
 fwrite(train, './project/volume/data/interim/train.csv')
 fwrite(test, './project/volume/data/interim/test.csv')
 fwrite(submit, './project/volume/data/interim/format.csv')
--- project/src/models/train_model.R
+++ project/src/models/train_model.R
@@ -1,24 +1,33 @@
+#takes the edited datasets from the interim folder
 test <- fread('./project/volume/data/interim/test.csv')
 train <- fread('./project/volume/data/interim/train.csv')
 submit <- fread('./project/volume/data/interim/format.csv')
 
+#saves training result column
 train_y <- train$result
 
+#creates a dummy variable to start prediction based on the row_sum column
 dummies <- dummyVars(result ~ ., data = train)
 train <- predict(dummies, newdata = train)
 test <- predict(dummies, newdata = test)
 
+#reformats the output into a data.table
 train <- data.table(train)
 train$result <- train_y
 test <- data.table(test)
 
+#uses a logistic model to fit the train data
 glm_model <- glm(result~., family = binomial, data = train)
 
+#saves dummy variable and model
 saveRDS(dummies, './project/volume/models/coin_glm.dummies')
 saveRDS(glm_model, './project/volume/models/coin_glm.model')
 
+#predicts the result on the test data
 test$result <- predict(glm_model, newdata = test, type = 'response')
 
+#puts the final result into the submission file format
 submit$result <- test$result
 
+#saves the final submission as a csv
 fwrite(submit, './project/volume/data/processed/submit_3.csv')
